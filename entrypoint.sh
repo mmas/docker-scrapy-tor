5efc3df8c4ba73147660897fb8d28b62caebc33f
mt <modesto@mtdev.co>
2015-12-23 14:31:50 +0000

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,9 @@
 service tor start
 service privoxy start
 
-if [ $1 == 'crawl' ]; then
+if [ $1 ] && [ $1 == 'crawl' ]; then
     scrapy crawl $2
-elif [ $1 == 'shell' ]; then
+elif [ $1 ] && [ $1 == 'shell' ]; then
     scrapy shell $2
 else
     /bin/bash
